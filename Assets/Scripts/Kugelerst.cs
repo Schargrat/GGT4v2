4cdb8660ab4b669b3a67e3d6b4705931e5dcc316
Schargrat <bastianvomdach@gmx.de>
2017-10-21 22:37:38 +0200

    jou

--- Assets/Scripts/Kugelerst.cs
+++ Assets/Scripts/Kugelerst.cs
@@ -2,23 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boccia : MonoBehaviour {
-    public Renderer rend;
-    public Color altColor = Color.red;
+public class Kugelerst : MonoBehaviour {
 
+    public GameObject bocciakugel;
+    public float v=1;
+    private int count = 0;
     // Use this for initialization
     //erstellt eine Kugel auf einer bestimmten Position mit den Eigenschaften eines festen Körpers also Kollider, rigitbody etc.
     //nur für den einen Spieler
     void Start () {
-        rend = GetComponent<Renderer>();
-        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        sphere.transform.position = new Vector3((float)4.09, (float)2.3, (float)0.28);
-        sphere.transform.localScale = new Vector3((float)0.3, (float)0.3, (float)0.3);
-        rend.material.color = altColor;
+       
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (count == 4)
+        {
+            
+        }
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            count++;
+            werfenderkugel();
+        }
 	}
+    void werfenderkugel()
+    {
+        GameObject kugel = Instantiate(bocciakugel, transform.position, Quaternion.identity);
+        Vector3 force = Camera.main.transform.forward.normalized;
+        force = force * v;
+        kugel.GetComponent<Rigidbody>().AddForce(force);
+    }
 }
